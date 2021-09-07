b7362d03c45ae0d8e9e2cc2c897df1ee3efec11b
Daniel Paul Wilkinson <danielpaulwilkinson.developer@gmail.com>
2021-09-07 22:01:20 +0100

    update

---
+++ src/router.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/youtube";
+import NotFound from "./views/youtube";
+import About from "./views/youtube";
+import Reddit from "./views/reddit";
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    { path: "/", name: "home", component: Home },
+    { path: "/youtube", name: "youtube", component: Home },
+    { path: "/reddit", name: "reddit", component: Reddit },
+    { path: "/about", name: "about", component: About },
+    { path: "*", component: NotFound },
+  ],
+});
